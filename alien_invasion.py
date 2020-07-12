d33a98c948d59d46bc3482dfa987ce9b41648bcc
sdkcouto <matheuscouto.eng@gmail.com>
2020-07-12 15:33:12 -0300

    pygame02

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,16 +1,19 @@
-import sys
-
 import pygame
+
+from settings import Settings
+
+from ship import Ship
+
+import game_functions as gf
 
 def run_game():
     pygame.init()
-    screen = pygame.display.set_mode((1200,800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-
+    ship = Ship(ai_settings, screen)
     while True:
-        for event in pygame.event.get():
-            if event.type ==pygame.QUIT:
-                sys.exit()
-        pygame.display.flip()
-
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
 run_game()
---
+++ settings.py
@@ -0,0 +1,6 @@
+class Settings():
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (0, 0, 255)
+        self.ship_speed_factor = 1.5
